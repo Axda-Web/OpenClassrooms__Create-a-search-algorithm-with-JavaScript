f5621b600b3611cc4f874cd8116b7ce78aa5b49f
Axda <axda.web@gmail.com>
2022-04-12 16:08:19 +0200

    Added complementary functionnality: search by ingredients & ustensils

--- scripts/models/apiManager.js
+++ scripts/models/apiManager.js
@@ -4,9 +4,11 @@ import recipes from "../../data/recipes.js"
 export default class apiManager {
     constructor(){
         this.data = []
+        this.allingredients = []
+        this.allustensils = []
     }
     
-    static init() {
+    static async getData() {
         this.data = recipes
     }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -5,20 +5,28 @@ import displayRecipeCard from '../utils/displayRecipeCard.js'
 const searchTerm = document.getElementById('search')
 
 //Initialisation des recettes
-apiManager.init()
+apiManager.getData()
 
 //Affichage des recettes
 displayRecipeCard(apiManager.data)
 
 
-//Gestion input events de la search bar
-function handleInputChange(e) {
+function handleInputChange(e){
     if (e.target.value.length > 2){
-        const filteredData = apiManager.data.filter( recipe => recipe.name.toLowerCase().includes(e.target.value.toLowerCase()))
+        let filteredData = filterRecipes(e.target.value.toLocaleLowerCase())
         displayRecipeCard(filteredData)
     } else {
         displayRecipeCard(apiManager.data)
-    }  
+    }
+}
+
+
+//Gestion input event de la search bar
+function filterRecipes(term) {
+    
+        return apiManager.data.filter( recipe => recipe.name.toLowerCase().includes(term) || 
+        recipe.description.toLowerCase().includes(term) ||
+        recipe.ingredients.some( ingredient => ingredient.ingredient.includes(term)))
 }
 
 searchTerm.addEventListener('input', handleInputChange)
